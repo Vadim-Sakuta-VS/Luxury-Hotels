44dad20def4e4076b1e1cd4aae1d97409ba0f9fc
Vadim <vadim10sakuta@gmail.com>
2021-02-15 16:48:43 +0300

    added section testimonials

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ function toggleMenu() {
     navigation.classList.toggle('nav--active');
     document.body.classList.toggle('overflow-hidden');
 }
+
+let swiper = new Swiper('.swiper-container', {
+    loop: true,
+    allowTouchMove: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1024: {
+            allowTouchMove: false,
+        }
+    }
+});
